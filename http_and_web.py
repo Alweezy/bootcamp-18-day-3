201a328bba550ab7bec5027b08085e98e4211e14
Alweezy <alvinmutisya@gmail.com>
2017-05-10 09:18:58 +0300

    Implement function to pass tests

--- http_and_web.py
+++ http_and_web.py
@@ -3,4 +3,17 @@ def get_name(artist_name):
 	:param artist_name:
 	:return: complete_url, name
 	"""
-	pass
+	if isinstance(artist_name, str):
+		name = '' + artist_name
+		name = ''.join(name.split())
+	else:
+		return 'Enter a valid artist_name: '
+	if name:
+		base_url = 'https://itunes.apple.com/search?term={}&entity=musicVideo'
+		artist = name.lower()
+		complete_url = base_url.format(artist)
+		return complete_url, name
+	return 'artist name is a requirement'
+
+
+print(get_name("Rihanna"))
