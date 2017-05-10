85efa23fed486322b706658bff9daae868d2022a
Alweezy <alvinmutisya@gmail.com>
2017-05-10 15:00:07 +0300

    Edit typo in base_url

--- http_and_web.py
+++ http_and_web.py
@@ -1,19 +1,16 @@
 def get_name(artist_name):
-	"""This function takes the name of artist whose music we want to look for in itunes
-	:param artist_name:
-	:return: complete_url, name
-	"""
-	if isinstance(artist_name, str):
-		name = '' + artist_name
-		name = ''.join(name.split())
-	else:
-		return 'Enter a valid artist_name: '
-	if name:
-		base_url = 'https://itunes.apple.com/search?term={}&entity=musicVideo'
-		artist = name.lower()
-		complete_url = base_url.format(artist)
-		return complete_url, name
-	return 'artist name is a requirement'
-
-
-print(get_name("Rihanna"))
+    """this function takes the name of artist whose music we want to look for in itunes
+    :param artist_name:
+    :return: complete_url, name
+    """
+    if isinstance(artist_name, str):
+        name = '' + artist_name
+        name = ''.join(name.split())
+    else:
+        return 'enter a valid artist_name: '
+    if name:
+        base_url = 'https://itunes.apple.com/search?term={}&entity=musicVideo'
+        artist = name.lower()
+        complete_url = base_url.format(artist)
+        return complete_url, name
+    return 'artist name is a requirement'
