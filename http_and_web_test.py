1becb9eb37846f7571c31f8c979ca2e9e33d678c
Alweezy <alvinmutisya@gmail.com>
2017-05-10 08:41:50 +0300

    Initial commit

--- http_and_web_test.py
+++ http_and_web_test.py
@@ -6,6 +6,10 @@ class TestGetName(TestCase):
     """Test the user input to make sure the name meets a certain criteria;
     that it is only a string.
     """
-    def test_artist_name_is_int(self):
+    def test_artist_name_is_not_int(self):
         getname = get_name(20)
+        self.assertEqual(getname, 'Enter a valid artist_name: ')
+
+    def test_artist_name_is_not_list(self):
+        getname = get_name([])
         self.assertEqual(getname, 'Enter a valid artist_name: ')
